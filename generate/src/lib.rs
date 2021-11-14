28d1f0bf485df2cc9439fecb256e72555f4d24c5
Pascal Sommer <p@pascalsommer.ch>
2021-11-14 16:07:19 +0100

    udpate gdal version

--- generate/src/lib.rs
+++ generate/src/lib.rs
@@ -1,11 +1,13 @@
+use gdal::LayerOptions;
+use rand::distributions::{Distribution, WeightedIndex};
 use std::path::Path;
-use rand::distributions::{
-    Distribution,
-    WeightedIndex,
-};
 
-pub fn rasterfile_to_shapefile<T, F, P>(mut weight_function: F, in_path: P, out_path: &str, nr_samples: usize)
-where
+pub fn rasterfile_to_shapefile<T, F, P>(
+    mut weight_function: F,
+    in_path: P,
+    out_path: &str,
+    nr_samples: usize,
+) where
     T: Copy + gdal::raster::GdalType,
     F: FnMut(T, &(usize, usize), &gdal::GeoTransform) -> f64,
     P: AsRef<Path>,
@@ -14,17 +16,24 @@ where
     let raster = in_dataset.rasterband(1).unwrap();
     let transform = in_dataset.geo_transform().unwrap();
 
-    let fun = |c: T, pos: &(usize, usize)| -> f64 {
-        weight_function(c, pos, &transform)
-    };
+    let fun = |c: T, pos: &(usize, usize)| -> f64 { weight_function(c, pos, &transform) };
     let samples = sample_on_raster(&raster, fun, nr_samples);
 
-    let mut out_dataset = gdal::Driver::get("ESRI Shapefile").unwrap().create_vector_only(out_path).unwrap();
+    let mut out_dataset = gdal::Driver::get("ESRI Shapefile")
+        .unwrap()
+        .create_vector_only(out_path)
+        .unwrap();
 
     let spatial_ref = in_dataset.spatial_ref().unwrap();
     let geometry_type = gdal::vector::OGRwkbGeometryType::wkbPoint;
 
-    let mut layer = out_dataset.create_layer("", Some(&spatial_ref), geometry_type).unwrap();
+    let mut layer = out_dataset
+        .create_layer(LayerOptions {
+            srs: Some(&spatial_ref),
+            ty: geometry_type,
+            ..Default::default()
+        })
+        .unwrap();
 
     for sample in &samples {
         let mut geometry = gdal::vector::Geometry::empty(geometry_type).unwrap();
@@ -39,11 +48,15 @@ pub fn index_to_coord((x, y): &(usize, usize), transform: &gdal::GeoTransform) -
 
     (
         transform[0] + fx * transform[1] + fy * transform[2],
-        transform[3] + fx * transform[4] + fy * transform[5]
+        transform[3] + fx * transform[4] + fy * transform[5],
     )
 }
 
-pub fn sample_on_raster<T, F>(raster_band: &gdal::raster::RasterBand, mut weight_function: F, nr_samples: usize) -> Vec<(usize, usize)>
+pub fn sample_on_raster<T, F>(
+    raster_band: &gdal::raster::RasterBand,
+    mut weight_function: F,
+    nr_samples: usize,
+) -> Vec<(usize, usize)>
 where
     T: Copy + gdal::raster::GdalType,
     F: FnMut(T, &(usize, usize)) -> f64,
@@ -68,7 +81,9 @@ where
 
         weight_per_row.push(0.0);
 
-        raster_band.read_into_slice((0, y as isize), (width, 1), (width, 1), &mut buffer, None).unwrap();
+        raster_band
+            .read_into_slice((0, y as isize), (width, 1), (width, 1), &mut buffer, None)
+            .unwrap();
         for (x, &entry) in buffer.iter().enumerate() {
             let weight = weight_function(entry, &(x, y));
             weight_per_row[y] += weight;
@@ -88,7 +103,9 @@ where
 
         let y = dist.sample(&mut rng);
 
-        raster_band.read_into_slice((0, y as isize), (width, 1), (width, 1), &mut buffer, None).unwrap();
+        raster_band
+            .read_into_slice((0, y as isize), (width, 1), (width, 1), &mut buffer, None)
+            .unwrap();
 
         for (x, &entry) in buffer.iter().enumerate() {
             weights[x] = weight_function(entry, &(x, y));
